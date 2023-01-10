6f8a6b2b94494a13fe63d09f7f9aa955c5ddeadd
shreyakolhe99 <122141806+shreyakolhe99@users.noreply.github.com>
2023-01-10 12:22:59 +0530

    Update automation.sh

--- automation.sh
+++ automation.sh
@@ -54,8 +54,5 @@ if [[ -f ${docroot}/inventory.html ]]; then
 	echo -e "httpd-logs\t-\t${timestamp}\t-\ttar\t-\t${size}" >> ${docroot}/inventory.html
 fi
 
-# Create a cron job that runs service every minutes/day
-if [[ ! -f /etc/cron.d/automation ]]; then
-	#statements
-	echo "* * * * * root /root/automation.sh" >> /etc/cron.d/automation
-fi
+
+
